19fdcaad81694128228a7ccee170e9ca6562f8b5
Diogo <diogomatosemail@gmail.com>
2022-04-15 00:51:31 +0100

    Ficheiros html Doxygen

---
+++ doxygen/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['expo_0',['expo',['../expMat_8c.html#adc7913acd9d3f73a2a21031fca415b72',1,'expo(STACK *s):&#160;expMat.c'],['../stack_8h.html#adc7913acd9d3f73a2a21031fca415b72',1,'expo(STACK *s):&#160;expMat.c']]]
+];
