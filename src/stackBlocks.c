79f48a7ca12d6fa9967a2c659d059d3afb2f107a
Diogo <diogomatosemail@gmail.com>
2022-05-14 22:50:50 +0100

    Updates

--- src/stackBlocks.c
+++ src/stackBlocks.c
@@ -37,28 +37,7 @@ DADOS create_block(STACK* s, char* token)
     s->stack[s->sp] = d;
     return d;
 }
-/* DADOS create_block(STACK* s, char* token) */
-/* { */
-/*     char* block = malloc(sizeof(char) * BUFSIZ); */
 
-/*     int index = 0; */
-/*     ++token; */
-/*     while ( *token == ' ') */
-/*         ++token; */
-
-/*     for(int i = 0; *(token + i) != '}'; ++i) */
-/*     { */
-/*         *(block + index) = *(token + i); */
-/*         ++index; */
-/*     } */
-    
-/*     *(block + index - 1) = '\0'; */
-/*     DADOS d = {BLOCK, block}; */
-    
-/*     s->sp++; */
-/*     s->stack[s->sp] = d; */
-/*     return d; */
-/* } */
 /**
  * @brief Executa as operações contidas num bloco.
  * 
